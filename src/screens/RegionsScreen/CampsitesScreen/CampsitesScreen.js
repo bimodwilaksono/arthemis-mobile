4a653b65101daa27f5888d8038617d89b425f260
Galih Priambodo <galihpri99@gmail.com>
2023-05-28 00:08:05 +0700

    [ADD] group the campsites by province

--- src/screens/RegionsScreen/CampsitesScreen/CampsitesScreen.js
+++ src/screens/RegionsScreen/CampsitesScreen/CampsitesScreen.js
@@ -5,56 +5,7 @@ import { CampsiteCard } from './CampsiteCard';
 import { CampsiteDetail } from './CampsiteDetail';
 import { UseGetAllCampsites } from '../../../shared/query/campsitesQueries';
 
-// const cards = [
-//   { 
-//     photo: "https://media2.fdncms.com/northcoast/imager/u/original/6500641/cal4-cf2659682b34c569.jpg", 
-//     name: "Basketball Perimaria", 
-//     location: "Cikelinci, Jawa Utara",
-//     price: 3600,
-//     desc: "This campsite is well-known for its sightings of bigfoots. In fact, this area has been designed meticulously to provide you the ambience of North American forests, especially with the inclusions of bigfoots. They're hard to find, but if you manage to find ones, you can ask them for selfies for free!",
-//     address: "Cikelinci, Grobogan Regency, North Java 3600" 
-//   },
-//   { 
-//     photo: "https://i1.wp.com/doesgodexist.today/wp-content/uploads/2019/02/Crater-Meteor-Barringer.jpg?resize=1024%2C683&ssl=1", 
-//     name: "Boulevard van Diego", 
-//     location: "Citikus, North Java" ,
-//     price: 3600,
-//     desc: "This campsite is based on the Chicxulub crater.",
-//     address: "[redacted]" 
-//   },
-//   { 
-//     photo: "http://assets.climatecentral.org/images/uploads/news/10_29_14_upton_Antarctic_storm_winds_windy.jpg", 
-//     name: "Cochon le Bouffon", 
-//     location: "Cicelurut, North Java" ,
-//     price: 3600,
-//     desc: "[redacted]",
-//     address: "[redacted]" 
-//   },
-//   { 
-//     photo: "http://d.ibtimes.co.uk/en/full/1438133/mars-sunset.jpg", 
-//     name: "Boulevard Something", 
-//     location: "Citupai, North Java" ,
-//     price: 3600,
-//     desc: "[redacted]",
-//     address: "[redacted]" 
-//   },
-//   { 
-//     photo: "http://resourcefulenvironment.com/wp-content/uploads/2020/05/Fire-Pit.jpg", 
-//     name: "Boulevard Encore", 
-//     location: "Cibajing, North Java" ,
-//     price: 3600,
-//     desc: "[redacted]",
-//     address: "[redacted]" 
-//   },
-//   { 
-//     photo: "http://2.bp.blogspot.com/-4hX5dMkSXbM/VTiguJh4a3I/AAAAAAAAALs/HFaF2fuTMvc/s1600/goa-liang-bua.jpg", 
-//     name: "Boulevard Lagi", 
-//     location: "Cikelelawar, North Java" ,
-//     price: 3600,
-//     desc: "[redacted]",
-//     address: "[redacted]" 
-//   }
-// ];
+
 
 export const CampsitesScreen = (props) => {
   const [campsitesToSelect, setCampsitesToSelect] = React.useState(null);
@@ -89,16 +40,21 @@ export const CampsitesScreen = (props) => {
                 onPress={props.onPress}
               />
               <View style={styles.campsitesForRegion}>
-                {data.data.content.map((item,index)=> (
-                  <CampsiteCard 
-                    key={index}
-                    photo={item.photo  ? item.photo : photoAsPlaceholder }
-                    name={item.name}
-                    location={item.province}
-                    
-                    onPress={() => handleCardClick(item)}
-                  />
-                ))}
+              {data.data.content.map((item, index) => {
+                if (item.province === props.name) {
+                  return (
+                    <CampsiteCard
+                      key={index}
+                      photo={item.photo ? item.photo : photoAsPlaceholder}
+                      name={item.name}
+                      location={item.province}
+                      onPress={() => handleCardClick(item)}
+                    />
+                  );
+                } else {
+                  return null; // Optional: If you don't want to render anything when the condition is not met
+                }
+              })}
               </View>
             </ScrollView>
         ) : (<Text>Data not found</Text>)
